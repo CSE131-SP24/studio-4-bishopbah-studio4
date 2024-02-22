8169532fe50e6d0052ca248603cddc6843d6127e
caroline.bishop <caroline.bishop@localhost>
2024-02-22 11:20:10 -0600

    Studio 4 Complete

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,19 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+	StdDraw.setPenColor(200, 236, 250);
+	StdDraw.filledRectangle(0.5, 0.5, 0.45, 0.3);
+	// draw flag base
+	
+	StdDraw.setPenColor(247, 241, 146);
+	StdDraw.filledCircle(0.5, 0.5, 0.15);
+	StdDraw.setPenRadius(0.01);
+	StdDraw.line(0.5, 0.675, 0.5, 0.775);
+	StdDraw.line(0.5, 0.325, 0.5, 0.235);
+	StdDraw.setPenColor(23,92,41);
+	StdDraw.text(0.2, 0.5, "B");
+	StdDraw.text(0.5, 0.5, "B");
+	StdDraw.text(0.8, 0.5, "L");
+	
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,73 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		
+		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+	
+		if (shapeType.equals("rectangle"))
+		{
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			
+			if (isFilled == true)
+			{
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			}
+			
+			else 
+			{
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+		}
+			
+		else if (shapeType.equals("ellipse"))
+		{
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			
+			if (isFilled == true)
+			{
+				StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+			}
+			
+			else 
+			{
+				StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			}
+		}
+		
+		else
+		{
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			
+			double xCoor[] = {x1, x2, x3};
+			double yCoor[] = {y1, y2, y3};
+			
+			if (isFilled == true)
+			{
+				StdDraw.filledPolygon(xCoor, yCoor);
+			}
+			
+			else 
+			{
+				StdDraw.polygon(xCoor, yCoor);
+			}
+		}
 		
 	}
 }
